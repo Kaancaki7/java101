9ed4c3a6ef4c7400d306cbf7d11d1f683c6e9519
kaan <kaancaki0@gmail.com>
2022-10-08 17:45:35 +0300

    java101

---
+++ ElmasOlusturma.java
@@ -0,0 +1,31 @@
+package j101;
+
+import java.util.Scanner;
+
+public class ElmasOlusturma {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("bir değer giriniz:");
+        int n = sc.nextInt();
+
+        for (int i = 1;i <= n;i++) {
+            for (int j = 1;j <= (n-i);j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1;k <= (2*i) - 1;k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 1;i <= n;i++) {
+            for (int j = 1;j <= i;j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1;k <= (2 * n - (2 * i + 1));k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
