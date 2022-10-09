f94a5c02a4ee07774646144f48b14f9b3a1a1f52
kaan <kaancaki0@gmail.com>
2022-10-09 15:54:30 +0300

    java101

--- Giris.java
+++ Giris.java
@@ -4,15 +4,19 @@ import java.util.Scanner;
 
 public class Giris {
     public static void main(String[] args) {
-        int i=1, j=1;
-        while(i<3)
-        {
-            do
-            {
-                System.out.print(j + ",");
-                j++;
-            }while(j<4);
-            i++;
+      int n1;
+      int n2;
+
+      Scanner sc = new Scanner(System.in);
+        System.out.println("gir");
+        n1 = sc.nextInt();
+        System.out.println("gir");
+        n2 = sc.nextInt();
+        for (int i = 1;i <= (n1*n2);i++) {
+            if (n1 % i == 0 && n2 % i == 0) {
+                System.out.println(i);
+                break;
+            }
         }
     }
 }
